a5b92c82e870e0e0b5fb07e395a6c99f62e0e7d2
Yung-Hsiang Hu <iftnt1999@gmail.com>
2022-04-20 01:59:01 +0800

    util: add euclidean distance calculation

--- src/util.c
+++ src/util.c
@@ -1,7 +1,17 @@
+#include "util.h"
+
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 
 void pthread_perror(const char *prefix, int err) {
   fprintf(stderr, "%s: (%d)%s\n", prefix, err, strerror(err));
+}
+
+ull dist_euclidean(int *a, int *b, size_t n) {
+  ull sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += (a[i] - b[i]) * (a[i] - b[i]);
+  }
+  return sum;
 }
--- src/util.h
+++ src/util.h
@@ -1,1 +1,7 @@
+#include <stddef.h>
+
+typedef unsigned long long int ull;
+
 void pthread_perror(const char *prefix, int err);
+
+ull dist(int *a, int *b, size_t n);
