0f88180a076070f20fe387268b21552287ce6390
Yung-Hsiang Hu <iftnt1999@gmail.com>
2022-05-03 07:33:39 +0800

    main: add multi-thread support

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,9 +9,21 @@
 #include "list.h"
 #include "util.h"
 
+struct param {
+  dataset_t* train_data;
+  dataset_t* test_data;
+  int* pred_label;
+  ull job_begin;
+  ull job_len;
+  unsigned int k;
+};
+
+int read_dataset(dataset_t* dataset, char* file_name);
+void* thread_calc_knn(void* _param);
+void print_accuracy(int* true_label, int* pred_label, int len);
+
 int main(int argc, char* argv[]) {
-  FILE *train_fp, *test_fp;
-  csv_t train_csv, test_csv;
+  dataset_t train_data, test_data;
   unsigned int k, thread_num = 1;
 
   if (argc != 3 && argc != 2) {
@@ -23,20 +36,14 @@ int main(int argc, char* argv[]) {
   }
   printf("K: %d; #Threads: %d\n", k, thread_num);
 
-  train_fp = fopen("../data/diabetes_train.csv", "r");
-  test_fp = fopen("../data/diabetes_test.csv", "r");
-  if (!train_fp || !test_fp) exit(EXIT_FAILURE);
-  csv_fromfile(&train_csv, train_fp);
-  csv_fromfile(&test_csv, test_fp);
-  printf("Training dataset: %d records; %d fields\n", train_csv.len_recd,
-         train_csv.len_fld);
-  printf("Testing dataset: %d records; %d fields\n", test_csv.len_recd,
-         test_csv.len_fld);
-
-  dataset_t train_data, test_data;
-
-  dataset_fromcsv(&train_data, &train_csv);
-  dataset_fromcsv(&test_data, &test_csv);
+  if (read_dataset(&train_data, "../data/diabetes_train.csv") != 0 ||
+      read_dataset(&test_data, "../data/diabetes_test.csv") != 0) {
+    exit(EXIT_FAILURE);
+  }
+  printf("Training dataset: %d records; %d features\n", train_data.len_recd,
+         train_data.len_feature);
+  printf("Testing dataset: %d records; %d features\n", test_data.len_recd,
+         test_data.len_feature);
 
   struct timespec start, finish;
   double elapsed;
@@ -45,13 +52,25 @@ int main(int argc, char* argv[]) {
 
   printf("\n");
 
+  struct param* params =
+      (struct param*)malloc(sizeof(struct param) * thread_num);
+  pthread_t* thrds = (pthread_t*)malloc(sizeof(pthread_t) * thread_num);
   int* pred_labels = (int*)malloc(sizeof(int) * test_data.len_recd);
-  for (int i = 0; i < test_data.len_recd; i++) {
-    pred_labels[i] =
-        find_nearest_neighbor_class(test_data.feature[i], &train_data, k);
-    if (i % 100 == 0) {
-      fprintf(stderr, "\rProgress: %d %%", i * 100 / test_data.len_recd + 1);
-    }
+
+  // Launch threads
+  for (int i = 0; i < thread_num; i++) {
+    params[i].test_data = &test_data;
+    params[i].train_data = &train_data;
+    params[i].pred_label = pred_labels;
+    params[i].k = k;
+    params[i].job_len = test_data.len_recd / thread_num + 1;
+    params[i].job_begin = params[i].job_len * i;
+    pthread_create(&thrds[i], NULL, thread_calc_knn, (void*)&params[i]);
+  }
+
+  // Wait threads completed
+  for (int i = 0; i < thread_num; i++) {
+    pthread_join(thrds[i], NULL);
   }
 
   printf("\n");
@@ -60,27 +79,59 @@ int main(int argc, char* argv[]) {
   elapsed = (finish.tv_sec - start.tv_sec);
   elapsed += (finish.tv_nsec - start.tv_nsec) / 1.0E9;
   printf("Finished. Elapsed: %f seconds\n", elapsed);
+  print_accuracy(test_data.label, pred_labels, test_data.len_recd);
 
-  // Calculate the accuracy
-  int true_label, pred_label;
+  // Cleanup
+  free(params);
+  free(thrds);
+  free(pred_labels);
+  dataset_free(&train_data);
+  dataset_free(&test_data);
+
+  return EXIT_SUCCESS;
+}
+
+int read_dataset(dataset_t* dataset, char* file_name) {
+  FILE* fp = fopen(file_name, "r");
+  csv_t csv;
+  if (!fp) return -1;
+  csv_fromfile(&csv, fp);
+  dataset_fromcsv(dataset, &csv);
+
+  csv_free(&csv);
+  fclose(fp);
+  return 0;
+}
+
+void* thread_calc_knn(void* _param) {
+  struct param* param = (struct param*)_param;
+  ull i;
+  for (int index = 0; index < param->job_len; index++) {
+    i = index + param->job_begin;
+    if (i >= param->test_data->len_recd) break;
+    param->pred_label[i] = find_nearest_neighbor_class(
+        param->test_data->feature[i], param->train_data, param->k);
+
+    // Display progress
+    if (i % 100 == 0) {
+      fprintf(stderr, "\rProgress: %d %%", index * 100 / param->job_len + 1);
+    }
+  }
+
+  pthread_exit(0);
+}
+
+void print_accuracy(int* true_label, int* pred_label, int len) {
   ull correct = 0, error = 0;
-  for (int i = 0; i < test_data.len_recd; i++) {
-    true_label = test_data.label[i];
-    pred_label = pred_labels[i];
-    if (true_label == pred_label) {
+  for (int i = 0; i < len; i++) {
+    if (true_label[i] == pred_label[i]) {
       correct++;
     } else {
       error++;
     }
   }
-
   printf("Correct: %d; Error: %d; Accuracy: %f\n", correct, error,
          (float)(correct) / (correct + error));
 
-  dataset_free(&train_data);
-  dataset_free(&test_data);
-  csv_free(&train_csv);
-  csv_free(&test_csv);
-
-  return EXIT_SUCCESS;
+  return;
 }
